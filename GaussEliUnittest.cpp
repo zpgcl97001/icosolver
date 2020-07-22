8b8fe79d740291871aa4ed0cf893653e05ad6028
Lin <zpgcl97@gmail.com>
2020-07-23 00:10:29 +0800

    PLU mat is done

--- GaussEliUnittest.cpp
+++ GaussEliUnittest.cpp
@@ -2,3 +2,66 @@
 // Created by Lin on 2020/7/21.
 //
 
+#include "Matsolver.h"
+#include <iostream>
+
+int main(){
+    int Totalpoints = 3;
+    MatSol matsol;
+
+    double **AMat = new double *[Totalpoints];
+    double **BMat = new double *[Totalpoints];
+    double **CMat = new double *[Totalpoints];
+    for (int i = 0; i < Totalpoints; ++i) {
+        AMat[i] = new double[Totalpoints];
+        BMat[i] = new double[Totalpoints];
+        CMat[i] = new double[Totalpoints];
+    };
+
+    for (int i = 0; i <Totalpoints ; ++i) {
+        for (int j = 0; j <Totalpoints ; ++j) {
+            BMat[i][i] =  1;
+        }
+
+    }
+AMat[0][0]= 1;
+    AMat[0][1]=2;
+    AMat[0][2]=3;
+    AMat[1][0]=2;
+    AMat[1][1]=4;
+    AMat[1][2]=7;
+    AMat[2][0]=3;
+    AMat[2][1]=5;
+    AMat[2][2]=3;
+
+    matsol.PLUdecompose(BMat,CMat,AMat,3,0);
+    std::cout<<"P-----------------"<<std::endl;
+    for (int i = 0; i <Totalpoints ; ++i) {
+        for (int j = 0; j <Totalpoints ; ++j) {
+            std::cout<<BMat[i][j]<<std::endl;
+        }
+
+    }
+    std::cout<<"L-------------------"<<std::endl;
+    for (int i = 0; i <Totalpoints ; ++i) {
+        for (int j = 0; j <Totalpoints ; ++j) {
+            std::cout<<CMat[i][j]<<std::endl;
+        }
+
+    }
+
+    std::cout<<"U------------------"<<std::endl;
+    for (int i = 0; i <Totalpoints ; ++i) {
+        for (int j = 0; j <Totalpoints ; ++j) {
+            std::cout<<AMat[i][j]<<std::endl;
+        }
+
+    }
+
+
+    double pauss;
+    return 0;
+
+}
+
+
--- Matsolver.h
+++ Matsolver.h
@@ -5,97 +5,166 @@
 #ifndef ICOSOLVER_MATSOLVER_H
 #define ICOSOLVER_MATSOLVER_H
 
+#include <math.h>
+#include <iostream>
 
-
-Class MatSol{
+class MatSol{
 private:
-
+    double **PMat;
+    double **AMat;
+    double *bMat;
+    double *SolMat;
+    int sizen;
+    double **MatA;
+    double **MatB;
+    double **MatC;
+    int rowA;
+    int colA;
+    int rowB;
+    int colB;
+    int rowC;
+    int colC;
 public:
-void MatMul(double**A,int A_row,int A_col,double**B,int B_row,int B_col){
+    int b;
 
-    if(A_row != B_col){
-        std::cout<<"Arow not equal to Bcol"<<std::endl;
-        return ;
+void PLUdecompose(double **PMat,double **LMat,double **AMat,int sizen,int iter){
+
+    iter++;
+    double **Lninv = new double *[sizen];
+    double **NewPMat = new double *[sizen];
+    double **NewAMat = new double *[sizen];
+    double **NewLMat = new double *[sizen];
+    double **AMat_ =new double *[sizen];
+    for(int i = 0;i<sizen;++i){
+        NewPMat[i] = new double[sizen];
+        NewAMat[i] = new double[sizen];
+        Lninv[i]   = new double[sizen];
+        NewLMat[i] = new double[sizen];
+        AMat_[i]   = new double[sizen];
     }
-    static double**  C = new double*[A_row];
-    for (int i = 0; i <A_row ; ++i) {
-        C[i] = new double[B_col];
-    }
-    for (int i = 0; i <A_row ; ++i) {
-        for (int j = 0; j <A_col ; ++j) {
-            for(int k=0;k<B_col;++k){
-               C[i][k] += A[i][j]*B[j][k];
+
+    //copy AMat value
+    for (int u = 0; u <sizen ; ++u) {
+        for (int v = 0; v < sizen ; ++v) {
+            AMat_[u][v] = AMat[u][v];
+            if(u==v){
+                LMat[u][v]= 1;
+            }else {
+                LMat[u][v] = 0;
             }
-        }
-    }
+            }
 
-    return C;
-
-
-
-}
-
-void Gaussian_Elimination(double **PMat,double **AMat,double *bMat,double *SolMat,int size,int iter){
-
-    //Mat initialize
-    double **LMat = new double *[size];
-    double **NewPMat = new double *[size];
-    double **NewAMat = new double *[size];
-    for(int i = 0;i<size;++i){
-        NewPMat[i] = new double[size];
-        NewPMat[i] = new double[size];
-        LMat[i]    = new double[size];
     }
 
     //End initialize
-    for (int i=0;i<size; ++i) {
-        aii = AMat[i][i];
-
+    for (int i=0;i<sizen-1; ++i) {
+        double aii = AMat[i][i];
+        std::cout<<"aii="<<aii<<"      step="<<i<<std::endl;
         if(abs(aii - 0)<1e-5){
-            for (int j = 0; j <size ; ++k) {
-                for (int k = 0; k <size ; ++k) {
+            iter++;
+            for (int j = 0; j <sizen ; ++j) {
+                for (int k = 0; k <sizen ; ++k) {
                     if(j<i){
-                        NewAMat[j][k] = AMat[j][k];
-                        NewPMat[j][k] = PMat[j][k];
+                       AMat[j][k] = AMat_[j][k];
+                       NewPMat[j][k] = PMat[j][k];
                     }else{
-                        int I = i+ (j-i+1)%(size-i);
-                        NewAMat[j][k] = Amat[I][k];
+                        int I = i+ (j-i+1)%(sizen-i);
+                        AMat[j][k] = AMat_[I][k];
+                        NewPMat[j][k] = PMat[I][k];
                     }
-                    NewAMat[I][j] = AMat[i][j];
-                    NewPMat[I][j] = PMat[i][j];
+                    std::cout<<"Ajk"<<AMat[j][k]<<std::endl;
                 }
             }
-            for (int i = 0; i <size ; ++i) {
-                for (int j = 0; j <size ; ++j) {
-                    AMat[i][j] = NewAMat[i][j];
-                    PMat[i][j] = NewPMat[i][j];
+            for (int u = 0; u <sizen ; ++u) {
+                for (int v = 0; v <sizen ; ++v) {
+                    PMat[u][v] = NewPMat[u][v];
                 }
             }
-
+            if(iter<sizen){
+                MatSol::PLUdecompose(PMat,LMat,AMat,sizen,iter);
+            }else{
+                std::cout<<"Singular Matrix"<<std::endl;
+                return;
+            }
         }
 
-            for(int j =iter+1;j<size;++j) {
-                bMat[j] -= AMat[j][iter]/AMat[iter][iter]*bMat[iter];
-                for(k = iter+1;k<size;++k) {
-                    AMat[j][k] -= AMat[j][iter] / AMat[iter][iter] * AMat[iter][k];
+        for (int u = 0; u < sizen; ++u) {
+            for (int v = 0; v <sizen ; ++v) {
+                if(v==u){
+                    Lninv[u][v] = 1;
+                }
+                else{
+                    Lninv[u][v] = 0;
+                }
+
+            }
+        }
+            for(int j =i+1;j<sizen;++j) {
+                Lninv[j][i] = AMat[j][i]/ AMat[i][i];
+                std::cout<<j<<"   ani/aii="<<AMat[j][i]/ AMat[i][i]<<std::endl;
+                for(int k = 0;k<sizen;++k) {
+                    NewAMat[j][k] =AMat[j][k] - AMat[j][i] / AMat[i][i] * AMat[i][k];
                 }
             }
+        for (int u=i+1;u<sizen;++u){
+            for (int v=0;v<sizen;++v){
+                AMat[u][v]=NewAMat[u][v];
+            }
+        }
+            //intital MatC;
+            for (int u=0;u<sizen;++u){
+                for (int v=0;v<sizen;++v){
+                    NewLMat[u][v] = 0;
+                }
+            }
+            //calculate newL
+            MatSol::MatMul(LMat,Lninv,NewLMat,sizen,sizen,sizen,sizen,sizen,sizen);
+            std::cout<<"Lninv"<<std::endl;
+        for (int u=0;u<sizen;++u){
+            for (int v=0;v<sizen;++v){
+                std::cout<<Lninv[u][v]<<std::endl ;
+            }
+        }
+            //update LMat
+            for (int u=0;u<sizen;++u){
+                for (int v=0;v<sizen;++v){
+                    LMat[u][v] = NewLMat[u][v];
+                }
+            }
+
+
         }
     //Calculate P L U
+    delete Lninv;
+    delete NewAMat;
+    delete NewPMat;
+    delete NewLMat;
+    delete AMat_;
+    }
+    void MatMul(double **MatA,double **MatB,double **MatC,int rowA,int colA,int rowB,int colB,int rowC,int colC){
 
+        if(colA != rowB || rowA !=rowC || colB !=colC){
+            std::cout<<"MatMul error"<<std::endl;
+        }
+        for (int i=0;i<rowA;++i){
+            for (int j=0;j<colA;++j){
+                for (int k = 0; k < colB; ++k) {
+                    MatC[i][k] += MatA[i][j]*MatB[j][k];
+                }
+            }
+        }
     }
 
 
-
-}
-
-void Matlinechange
 
 
 
 
 
 };
+
+
+
 
 
 
